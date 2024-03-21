9ecd5d103de452120dbef45b439027b5baf6f587
marion-rld <rollando.marion@gmail.com>
2024-03-21 15:53:36 +0100

    Initial commit

---
+++ src/main/java/edu/mrollando/TP/TpJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.mrollando.TP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJavaApplication.class, args);
+	}
+
+}
